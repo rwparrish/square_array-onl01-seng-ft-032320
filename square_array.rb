36d6ac16e3daafde7dc2c25da8481106360cd5d7
Ryan Parrish <rino3717@gmail.com>
2020-03-24 20:37:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-      Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }    
+      Enumerator.new do |num|
+    array.each {  << e ** 2 }    
   end
   .take(array.length)
 end
